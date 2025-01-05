6bedfeae137538389df80e04ac02cbec42fbcfa2
SathiyanarayananSK <sathiyanarayanan.s1698@gmail.com>
2025-01-05 20:48:20 +1100

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,19 +1,21 @@
 import smtplib, ssl, os
 
-
 def send_email(user_input):
-    host = "smtp.gmail.com"
-    port = 465
+    """Send an email using SMTP with the provided input."""
+    host = "smtp.gmail.com"  # SMTP server for Gmail
+    port = 465  # Port for SSL
 
+    # Email credentials
     username = "ssk98.automations@gmail.com"
-    password = os.getenv("PortfolioEmailsAppPassword")
+    password = os.getenv("PortfolioEmailsAppPassword")  # Fetch password from environment variables
 
-    receiver = "ssk98.automations@gmail.com"
+    receiver = "ssk98.automations@gmail.com"  # Receiver email address
 
-    context = ssl.create_default_context()
+    context = ssl.create_default_context()  # SSL context for secure connection
 
-    message = user_input
+    message = user_input  # Email content
 
+    # Send the email using SMTP_SSL
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
